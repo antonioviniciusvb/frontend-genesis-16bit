ba596a48db640973670b10633763c0563bfae0d8
Antonio Bandeira <104794351+antonioviniciusvb@users.noreply.github.com>
2024-01-23 15:25:41 -0300

    :sparkles: feat: NavBar, Home e footer

---
+++ src/components/navBar/NavBar.tsx
@@ -0,0 +1,28 @@
+import { Link } from "react-router-dom"
+import { GameController } from '@phosphor-icons/react'
+
+function Navbar() {
+
+    return (
+        <>
+            <div className='w-full bg-sky-950 text-white flex justify-center py-4'>
+
+                <div className="container flex justify-between text-lg">
+                        <Link to="/home" className="text-2xl font-bold flex gap-4"><GameController size={48} weight='bold' />Genesis 16bit</Link>
+                    <div className='flex gap-4'>
+
+                        <Link to='/produtos' className='hover:underline'>Produtos</Link>
+                        <Link to='/categoria' className='hover:underline'>Categoria</Link>
+                        <Link to='/cadastroCategoria' className='hover:underline'>Cadastrar Categoria</Link>
+                        <div className='hover:underline'>Perfil</div>
+
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Navbar
+
+
